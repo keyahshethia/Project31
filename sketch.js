2a218b382974233ce8979581cbed9a436eca1041
keyahshethia <71205945+keyahshethia@users.noreply.github.com>
2020-11-09 10:43:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var particles;
+var plinkos;
+var divisions;
+var divisionHeight;
+
+
+function setup() {
+  createCanvas(480,800);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  
+  drawSprites();
+}
